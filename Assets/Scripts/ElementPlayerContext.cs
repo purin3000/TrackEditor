535d0ab14a23d9c23d2fd3bff211de36671ca04c
purin3000 <maabo@hi-ho.ne.jp>
2019-06-18 12:16:49 +0900

    エレメントから親のトラック、さらに親トラックを参照できるようにした

---
+++ Assets/Scripts/ElementPlayerContext.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace track_editor
+{
+    public class ElementPlayerContext
+    {
+        public TrackAsset asset;
+
+        public List<IElementPlayer> playElements = new List<IElementPlayer>();
+
+        Dictionary<string, TrackSerialize> tracks = new Dictionary<string, TrackSerialize>();
+        Dictionary<string, ElementSerialize> elements = new Dictionary<string, ElementSerialize>();
+
+        public ElementPlayerContext(TrackAsset asset)
+        {
+            this.asset = asset;
+
+            tracks.Clear();
+            elements.Clear();
+            playElements.Clear();
+
+            foreach (var trackSerialize in asset.rootTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
+            foreach (var trackSerialize in asset.gameObjectTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
+            foreach (var trackSerialize in asset.activationTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
+            foreach (var trackSerialize in asset.positionTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
+
+            foreach (var trackSerialize in asset.activationElements) { elements.Add(trackSerialize.uniqueName, trackSerialize); playElements.Add(new ActivationElementPlayer(trackSerialize)); }
+            foreach (var trackSerialize in asset.positionElements) { elements.Add(trackSerialize.uniqueName, trackSerialize); playElements.Add(new PositionElementPlayer(trackSerialize)); }
+
+            playElements = playElements.OrderBy(elem => elem.start).ToList();
+        }
+
+
+        public T GetTrack<T>(ElementSerialize elementSerialize) where T : TrackSerialize
+        {
+            return tracks[elementSerialize.parent] as T;
+        }
+
+        public T GetParentTrack<T>(ElementSerialize elementSerialize) where T : TrackSerialize
+        {
+            return tracks[tracks[elementSerialize.parent].parent] as T;
+        }
+    }
+
+
+    public interface IElementPlayer
+    {
+        int start { get; }
+        int length { get; }
+
+        void Apply(ElementPlayerContext context);
+    }
+
+    public class ActivationElementPlayer : IElementPlayer
+    {
+        ActivationElementSerialize elementSerialize;
+
+        public ActivationElementPlayer(ActivationElementSerialize trackSerialize) { this.elementSerialize = trackSerialize; }
+
+        public int start { get => elementSerialize.start; }
+        public int length { get => elementSerialize.length; }
+
+        public void Apply(ElementPlayerContext context)
+        {
+            var gameObjectTrack = context.GetParentTrack<GameObjectTrackSerialize>(elementSerialize);
+
+            Debug.LogFormat("Activation start:{0} {1}", start, gameObjectTrack.go);
+        }
+    }
+
+    public class PositionElementPlayer : IElementPlayer
+    {
+        PositionElementSerialize elementSerialize;
+
+        public PositionElementPlayer(PositionElementSerialize trackSerialize) { this.elementSerialize = trackSerialize; }
+
+        public int start { get => elementSerialize.start; }
+        public int length { get => elementSerialize.length; }
+
+        public void Apply(ElementPlayerContext context)
+        {
+            var gameObjectTrack = context.GetParentTrack<GameObjectTrackSerialize>(elementSerialize);
+
+            Debug.LogFormat("Position start:{0}", start);
+        }
+    }
+}
--- Assets/Scripts/TrackAssetPlayer.cs
+++ Assets/Scripts/TrackAssetPlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 namespace track_editor
 {
@@ -13,60 +12,14 @@ namespace track_editor
 
         public bool IsPlaying { get => 0 <= playCurrent; }
 
-        public bool IsPlayEnd { get => playCurrent == playElements.Count; }
+        public bool IsPlayEnd { get => (playContext != null) && playCurrent == playContext.playElements.Count; }
 
-        Dictionary<string, TrackSerialize> tracks = new Dictionary<string, TrackSerialize>();
-        Dictionary<string, ElementSerialize> elements = new Dictionary<string, ElementSerialize>();
-
-        List<IElementPlayer> playElements = new List<IElementPlayer>();
+        ElementPlayerContext playContext = null;
 
         int playCurrent = -1;
 
         float time = 0.0f;
         float speed = 1.0f;
-
-
-
-
-
-        interface IElementPlayer
-        {
-            int start { get; }
-            int length { get; }
-
-            void Apply();
-        }
-
-        class ActivationElementPlayer : IElementPlayer
-        {
-            ActivationElementSerialize trackSerialize;
-
-            public ActivationElementPlayer(ActivationElementSerialize trackSerialize) { this.trackSerialize = trackSerialize; }
-
-            public int start { get => trackSerialize.start; }
-            public int length { get => trackSerialize.length; }
-
-            public void Apply()
-            {
-                Debug.LogFormat("Activation start:{0}", start);
-            }
-        }
-
-        class PositionElementPlayer : IElementPlayer
-        {
-            PositionElementSerialize trackSerialize;
-
-            public PositionElementPlayer(PositionElementSerialize trackSerialize) { this.trackSerialize = trackSerialize; }
-
-            public int start { get => trackSerialize.start; }
-            public int length { get => trackSerialize.length; }
-
-            public void Apply()
-            {
-                Debug.LogFormat("Position start:{0}", start);
-            }
-        }
-
 
 
         private void Play(TrackAsset asset)
@@ -79,19 +32,7 @@ namespace track_editor
             playCurrent = 0;
             time = 0.0f;
 
-            tracks.Clear();
-            elements.Clear();
-            playElements.Clear();
-
-            foreach (var trackSerialize in asset.rootTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
-            foreach (var trackSerialize in asset.gameObjectTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
-            foreach (var trackSerialize in asset.activationTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
-            foreach (var trackSerialize in asset.positionTracks) { tracks.Add(trackSerialize.uniqueName, trackSerialize); }
-
-            foreach (var trackSerialize in asset.activationElements) { elements.Add(trackSerialize.uniqueName, trackSerialize); playElements.Add(new ActivationElementPlayer(trackSerialize)); }
-            foreach (var trackSerialize in asset.positionElements) { elements.Add(trackSerialize.uniqueName, trackSerialize); playElements.Add(new PositionElementPlayer(trackSerialize)); }
-
-            playElements = playElements.OrderBy(elem => elem.start).ToList();
+            playContext = new ElementPlayerContext(asset);
         }
 
         void Start()
@@ -104,16 +45,16 @@ namespace track_editor
         // Update is called once per frame
         void Update()
         {
-            if (IsPlaying) {
+            if (playContext != null && IsPlaying) {
 
-                while (playCurrent < playElements.Count) {
-                    var element = playElements[playCurrent];
+                while (playCurrent < playContext.playElements.Count) {
+                    var element = playContext.playElements[playCurrent];
 
                     if (time * 60.0f < element.start) {
                         break;
                     }
 
-                    element.Apply();
+                    element.Apply(playContext);
                     ++playCurrent;
                 }
 
